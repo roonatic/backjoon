1a7a3aa60f620fc41fe48da8fbd4c20918d6a6ca
roonatic <sudo.rwx777@gmail.com>
2022-03-18 22:56:30 +0900

    DFS 구현 완성 및 전체적인 코드 수정

--- backjoon/1260 - DFS와 BFS.cpp
+++ backjoon/1260 - DFS와 BFS.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -11,8 +12,12 @@ int main()
 	int v; //탐색 시작할 정점 번호
 
 	cin >> n >> m >> v;
-	vector<int>* graph = new vector<int>[n+1];
-	bool* visited = new bool[n+1];
+	vector<int>* graph = new vector<int>[n+1]; //그래프
+	bool* visited = new bool[n+1]; //방문했는지 체크하기 위한 배열
+
+	//방문 체크 배열 초기화
+	for (int i = 0; i < n + 1; i++)
+		visited[i] = false;
 
 	//그래프 구현
 	int a;
@@ -21,29 +26,41 @@ int main()
 	{
 		cin >> a >> b;
 		graph[a].push_back(b);
+		graph[b].push_back(a); //양방향 그래프기 때문에 반대의 경우도 추가
 	}
 
+	//오름차순으로 정렬
+	for (int i = 0; i < n+1; i++)
+	{
+		for (int j = 0; j < graph[i].size(); j++)
+			sort(graph[i].begin(), graph[i].end());
+	}
 
-	
 	//DFS 구현 - Stack 사용
 	stack<int> DFS;
 	DFS.push(v);
 	visited[v] = true;
+	cout << DFS.top() << ' ';
+
 	while (!DFS.empty())
 	{
-		int cur = DFS.top();
-		cout << cur << ' ';
+		int cur = DFS.top(); //현재 위치
+		DFS.pop();
 		for (int i = 0; i < graph[cur].size(); i++)
 		{
 			int now = graph[cur][i];
 			if (!visited[now])
 			{
+				cout << now << ' ';
+				DFS.push(cur);
 				DFS.push(now);
 				visited[now] = true;
 				break;
 			}
 		}
 	}
+
+	cout << endl;
 
 	//BFS 구현 - Queue 사용
 	//앞에서 쓴 visited 초기화
@@ -53,18 +70,19 @@ int main()
 	queue<int> BFS;
 	BFS.push(v);
 	visited[v] = true;
+	cout << BFS.front() << ' ';
 	while (!BFS.empty())
 	{
 		int cur = BFS.front();
-		BFS.pop(); //방문했으므로 true로
-		cout << cur << ' ';
+		BFS.pop(); 
 		for (int i = 0; i < graph[cur].size(); i++)
 		{
 			int now = graph[cur][i];
-			if (!visited[now])
+			if (!visited[now]) //그 전에 방문하지 않았을 경우에
 			{
+				cout << now << ' ';
 				BFS.push(now);
-				visited[now] = true;
+				visited[now] = true; //방문했으므로 true로
 			}
 		}
 	}
