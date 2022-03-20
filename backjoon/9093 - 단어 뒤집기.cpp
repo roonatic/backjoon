33377956bfcc1ba08e181aa8ef5061959b321327
roonatic <sudo.rwx777@gmail.com>
2022-03-20 19:04:10 +0900

    주석 추가

--- backjoon/9093 - 단어 뒤집기.cpp
+++ backjoon/9093 - 단어 뒤집기.cpp
@@ -8,7 +8,8 @@ int main()
 {
 	int n;
 	cin >> n;
-	cin.ignore();
+	cin.ignore(); //입력 버퍼에 개행문자가 남아있기 떄문에 버퍼 클리어 해줌
+
 	while(n--)
 	{
 		string input;
@@ -17,21 +18,21 @@ int main()
 
 		for (int i = 0; i < input.size(); i++)
 		{
-			if (input[i] == ' ')
+			if (input[i] == ' ') //공백을 맞이하면
 			{
-				while (!reverse.empty())
+				while (!reverse.empty()) //스택 비움
 				{
-					cout << reverse.top();
-					reverse.pop();
+					cout << reverse.top(); //먼저 맨 위의 단어를 출력하고
+					reverse.pop(); //단어 제거
 				}
-				cout << ' ';
+				cout << ' '; //이후 공백 출력
 			}
-			else if (i == input.size() - 1)
+			else if (i == input.size() - 1) //문장의 끝 단어면
 			{
-				reverse.push(input[i]);
-				while (!reverse.empty())
+				reverse.push(input[i]); //해당 단어를 먼저 스택에 추가한 후
+				while (!reverse.empty()) //스택이 비워질때까지 맨 위의 단어들을 입력 = 단어들이 뒤집어짐
 				{
-					cout << reverse.top();
+					cout << reverse.top(); 
 					reverse.pop();
 				}
 				break;
