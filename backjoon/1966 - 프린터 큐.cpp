a5a2a371ec5384b84287b0bf54767ea0b845c484
roonatic <sudo.rwx777@gmail.com>
2022-03-26 22:59:48 +0900

    코드 수정 및 주석 추가 / 우선순위 큐 공부

--- backjoon/1966 - 프린터 큐.cpp
+++ backjoon/1966 - 프린터 큐.cpp
@@ -1,31 +1,34 @@
+//
+//우선순위 큐 문제
+//1966 - 프린터 큐
+//우선순위 큐 STL priority_queue
 #include <iostream>
 #include <queue>
 using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
-
-	while (n--)
+	queue<int> printer;
+	int cmd; //테스트케이스의 수
+	cin >> cmd;
+	while (cmd--)
 	{
-		queue<int> printer;
-		int print;
-		int what;
-		int cnt = 0;
-		int cmp;
-		cin >> print >> what;
-		
-		while (!cin.eof())
+		int n; //문서의 개수
+		int m; //몇번째로 인쇄되었는지 궁금한 문서
+		int target;
+		cin >> n >> m;
+		while (n--)
 		{
 			int doc;
 			cin >> doc;
 			printer.push(doc);
-			cnt++;
-			if (cnt == what)
-				cmp = what;
+			if (printer.size() == m)
+				target = printer.back();
 		}
-		
+
+		if (m == 0)
+			target = printer.front();
+
 
 		
 	}
