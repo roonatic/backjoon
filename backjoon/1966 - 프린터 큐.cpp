32afdf43167eb59f05b1413d2664f7df08731814
roonatic <sudo.rwx777@gmail.com>
2022-03-27 22:11:06 +0900

    코드 완성 - 우선순위 큐 및 pair

--- backjoon/1966 - 프린터 큐.cpp
+++ backjoon/1966 - 프린터 큐.cpp
@@ -1,4 +1,3 @@
-//
 //우선순위 큐 문제
 //1966 - 프린터 큐
 //우선순위 큐 STL priority_queue
@@ -8,27 +7,50 @@ using namespace std;
 
 int main()
 {
-	queue<int> printer;
 	int cmd; //테스트케이스의 수
 	cin >> cmd;
 	while (cmd--)
 	{
+		//초기화
+		queue<pair<int, int>> printer; //문서 큐
+		priority_queue<int> prior; //인쇄 순서 우선순위 큐
+
 		int n; //문서의 개수
 		int m; //몇번째로 인쇄되었는지 궁금한 문서
-		int target;
+		int printcnt = 0; // 몇 번째로 인쇄되었는지
 		cin >> n >> m;
-		while (n--)
+		
+		for(int i=0;i<n;i++)
 		{
 			int doc;
 			cin >> doc;
-			printer.push(doc);
-			if (printer.size() == m)
-				target = printer.back();
+			printer.push(make_pair(i, doc)); //(n번째, 문서)
+			prior.push(doc); //우선순위 삽입
 		}
 
-		if (m == 0)
-			target = printer.front();
+		while (!printer.empty())
+		{
+			int index = printer.front().first; //(n번째)
+			int priority = printer.front().second; //문서
 
+			if (prior.top() > priority) //우선순위가 높으면
+			{
+				printer.push(printer.front()); //앞에 있던 것을 뒤에 삽입
+				printer.pop(); //삽입 후에 앞의 원소를 빼줌
+			}
+			else
+			{
+				printcnt++;
+				printer.pop();
+				prior.pop();
+
+				if (index == m) //만약 찾으려고 했던 문서 발견시
+				{
+					cout << printcnt << endl; //출력
+					break;
+				}
+			}
+		}
 
 		
 	}
