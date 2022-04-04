0aefe983e09f54bfa1299b1de9ada97f586780f7
roonatic <sudo.rwx777@gmail.com>
2022-04-05 00:15:53 +0900

    수정필요

--- backjoon/17413 - 단어 뒤집기2.cpp
+++ backjoon/17413 - 단어 뒤집기2.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <stack>
-#include <queue>
+#include <string>
 using namespace std;
 
 int main()
 {
 	stack<char> r;
-	queue<char> t;
 	string str;
-	cin >> str;
+	getline(cin, str);
 
 	string tmpt = "";
 	for (int i = 0; i < str.size(); i++)
@@ -30,25 +29,34 @@ int main()
 		}
 		else if (/*str[i] >= 48 && str[i] <= 57 &&*/ str[i] >= 97 && str[i] <= 122)
 		{
-			while (/*(!(str[i] == 32)) ||*/ (!(str[i] == 60)))
+			while ((!(str[i] == 32)) || (!(str[i] == 60)))
 			{
-				r.push(str[i]);
-				i++;
-			}
-
-			if (str[i] == 32 || str[i] == 60)
-			{
-				while (!r.empty())
+				if (str[i] == 32 || str[i] == 60)
 				{
-					cout << r.top();
-					r.pop();
+					while (!r.empty())
+					{
+						cout << r.top();
+						r.pop();
+					}
+					if (i == str.size())
+						break;
+					else
+					{ 
+						i--;
+						break;
+					}
+				}
+				else
+				{
+					r.push(str[i]);
+					i++;
 				}
 			}
 		}
 		else if (str[i] == 32)
 		{
 			if (i != str.size())
-				cout << "";
+				cout << ' ';
 			else
 				break;
 		}
