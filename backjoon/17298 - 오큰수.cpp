7d2db37848744e5c4450e5c46ed3f31dbf01fa8e
roonatic <sudo.rwx777@gmail.com>
2022-04-11 23:35:50 +0900

    주석 추가

--- backjoon/17298 - 오큰수.cpp
+++ backjoon/17298 - 오큰수.cpp
@@ -2,7 +2,6 @@
 #include <stack>
 using namespace std;
 
-
 int main()
 {
 	int n;
@@ -10,29 +9,29 @@ int main()
 	
 	int m;
 
-	int* arr = new int[n];
-	stack<int> NGE;
-	for (int i = 0; i < n; i++)
+	int* arr = new int[n]; //입력배열 동적할당
+	stack<int> NGE; //계산을 위한 스택
+	for (int i = 0; i < n; i++) //먼저 배열에 입력값 추가
 	{
 		cin >> m;
 		arr[i] = m;
 	}
 
-	int* ans = new int[n];
-	for (int i = n - 1; i >= 0; i--)
+	int* ans = new int[n]; //정답 배열 동적할당
+	for (int i = n - 1; i >= 0; i--) //입력배열의 맨 뒤에서부터 시작
 	{
-		while (!NGE.empty() && NGE.top() <= arr[i])
-			NGE.pop();
+		while (!NGE.empty() && NGE.top() <= arr[i]) // 계산 스택이 비어있지 않고 top이 기준값보다 작으면
+			NGE.pop(); //top을 빼줌 
 	
-		if (NGE.empty())
-			ans[i] = -1;
+		if (NGE.empty()) //시작값이 맨 오른쪽에 있으면 더 이상 오른쪽은 없기 때문에
+			ans[i] = -1; //해당값의 정답은 -1이 된다
 		else
-			ans[i] = NGE.top();
+			ans[i] = NGE.top(); // top이 기준값보다 크면 정답은 top 
 
-		NGE.push(arr[i]);
+		NGE.push(arr[i]); //기준값을 넣어줘서 앞에서 계산하기 위한 것을 저장
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) //정답 출력
 			cout << ans[i] << ' ';
 
 }
