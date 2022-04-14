cd78166ffbad28f9776c1ee5f410e789944b556e
roonatic <sudo.rwx777@gmail.com>
2022-04-14 23:55:56 +0900

    후위 표기식2 수정

--- backjoon/1935 - 후위 표기식2.cpp
+++ backjoon/1935 - 후위 표기식2.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main()
 {
 	int n, m;
 	string cmd;
-	stack<int> num;
+	stack<double> num;
 	stack<char> cal;
 	cin >> n;
-	int* arr = new int[n];
+	int* arr = new int[n+1];
 
 	cin >> cmd;
 
@@ -20,13 +21,54 @@ int main()
 		arr[i] = m;
 	}
 
+	double result = 0;
 	int j = 0;
-	for (int i = 0; i <= cmd.size(); i++)
+	for (int i = 0; i < cmd.size(); i++)
 	{
 		if (cmd[i] >= 65 && cmd[i] <= 90)
 		{
-			cal.push(cmd[i]);
+			num.push(arr[j]);
 			j++;
 		}
+		else if (cmd[i] == '+' || cmd[i] == '-')
+		{
+			if (cal.top() == '*' || cal.top() == '/')
+			{
+				char temp = cal.top();
+				cal.pop();
+				cal.push(cmd[i]);
+				cal.push(temp);
+			}
+			else
+				cal.push(cmd[i]);
+		}
+		else if (cmd[i] == '*' || cmd[i] == '/')
+			cal.push(cmd[i]);
+		
+		if (i == cmd.size() - 1 || (cmd[i] == ))
+		{
+
+			while (num.size() != 1 && !cal.empty())
+			{
+				double num1 = num.top();
+				num.pop();
+				double num2 = num.top();
+				num.pop();
+
+				if (cal.top() == '+')
+					result = num1 + num2;
+				else if (cal.top() == '-')
+					result = num1 - num2;
+				else if (cal.top() == '*')
+					result = num1 * num2;
+				else if (cal.top() == '/')
+					result = num1 / num2;
+
+				cal.pop();
+				num.push(result);
+			}
+		}
 	}
+
+	printf("%.2f",num.top());
 }
