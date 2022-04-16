3144bc6c81e1750659b93032b25e7dbf46728656
roonatic <sudo.rwx777@gmail.com>
2022-04-16 15:17:20 +0900

    후위 표기식2 완성

--- backjoon/1935 - 후위 표기식2.cpp
+++ backjoon/1935 - 후위 표기식2.cpp
@@ -8,28 +8,28 @@ int main()
 {
 	int n;
 	double m;
+	double result = 0;
 	string cmd;
 	stack<double> num;
 	stack<char> cal;
 	cin >> n;
-	int *arr = new int[n+1];
+	int* arr = new int[n + 1];
 
 	cin >> cmd;
 
+	//A->0 B->1
 	for (int i = 0; i < n; i++)
 	{
 		cin >> m;
-		arr[] = m;
-		cnt++;
+		arr[i] = m;
 	}
 
-	double result = 0;
 	for (int i = 0; i < cmd.size(); i++)
 	{
-		if (cmd[i] >= 65 && cmd[i] <= 90)
+		if (cmd[i] >= 65 && cmd[i] <= 90) //¾ËÆÄºªÀÌ¸é
 		{
-			int j = cmd[i] - 65;
-			num.push(arr[j]);
+			double tmpt = arr[cmd[i] - 65];
+			num.push(tmpt);
 		}
 		else if (cmd[i] == '+' || cmd[i] == '-')
 		{
@@ -45,31 +45,27 @@ int main()
 		}
 		else if (cmd[i] == '*' || cmd[i] == '/')
 			cal.push(cmd[i]);
-		
-		if (i == cmd.size() - 1 || (cmd[i+1] >= 65 && cmd[i+1] <= 90) && (cmd[i] == '*' || cmd[i] == '/' || cmd[i] == '+' || cmd[i] == '-'))
+
+		while (num.size() != 1 && !cal.empty())
 		{
+			double num2 = num.top();
+			num.pop();
+			double num1 = num.top();
+			num.pop();
 
-			while (num.size() != 1 && !cal.empty())
-			{
-				double num2 = num.top();
-				num.pop();
-				double num1 = num.top();
-				num.pop();
+			if (cal.top() == '+')
+				result = num1 + num2;
+			else if (cal.top() == '-')
+				result = num1 - num2;
+			else if (cal.top() == '*')
+				result = num1 * num2;
+			else if (cal.top() == '/')
+				result = num1 / num2;
 
-				if (cal.top() == '+')
-					result = num1 + num2;
-				else if (cal.top() == '-')
-					result = num1 - num2;
-				else if (cal.top() == '*')
-					result = num1 * num2;
-				else if (cal.top() == '/')
-					result = num1 / num2;
-
-				cal.pop();
-				num.push(result);
-			}
+			cal.pop();
+			num.push(result);
 		}
 	}
 
-	printf("%.2lf",num.top());
+	printf("%.2lf", num.top());
 }
