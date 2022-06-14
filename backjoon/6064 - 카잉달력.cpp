a1e793efaad353c613026e3e46b12b3740af62ba
roonatic <sudo.rwx777@gmail.com>
2022-06-14 21:00:48 +0900

    카잉달력 완성

--- backjoon/6064 - 카잉달력.cpp
+++ backjoon/6064 - 카잉달력.cpp
@@ -1,42 +1,66 @@
+//분류에는 브루트포스라고 되어있지만 수학을 이용하여 푸는 문제
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b);
+int lcm(int a, int b);
+
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	int t;
 	cin >> t;
-	while (t--)
+	for(int k=0;k<t;k++)
 	{
 		int M, N, x, y;
 		cin >> M >> N >> x >> y;
 
-		int tx = 1, ty = 1;
-		int cnt = 1;
-
-
-		while (1)
+		int last = lcm(M, N);
+		int i = x;
+		int j = 0;
+		while (i <= last)
 		{
-			if (tx < M)
-				tx++;
-			else
-				tx = 1;
-
-			if (ty < N)
-				ty++;
-			else
-				ty = 1;
-
-			cnt++;
-
-			if ((x == tx) && (y == ty))
+			j = i % N;
+			if (j == 0)
+				j = N;
+			if (j == y)
 				break;
-			else if ((M == tx) && (N == ty))
-			{
-				cnt = -1;
-				break;
-			}
+
+			i += M;
 		}
-		cout << cnt << "\n";
+		if (i > last)
+			cout << -1 << "\n";
+		else
+			cout << i << "\n";
 	}
 
+	return 0;
+}
+
+int gcd(int a, int b)
+{
+	if (a < b)
+	{
+		int tmpt = a;
+		a = b;
+		b = tmpt;
+	}
+
+	int c;
+	while (b)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	
+	return a;
+}
+
+int lcm(int a, int b)
+{
+	return a * b / gcd(a, b);
 }
